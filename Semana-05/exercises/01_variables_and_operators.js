f1a436bed869191fb9752e0c17f09b96218b55ef
Ariana Maldonado <arii.maldonado97@gmail.com>
2022-04-17 20:29:13 -0300

    BaSP-2022: variables and operators solution

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,15 @@
+// Exercise-1a
+var num1 = 5;
+var num2 = 10;
+var result = num1 + num2;
+console.log("Result-1a", result);
+// Exercise-1b
+var str1 = "My name is";
+var str2 = " Ariana Maldonado.";
+var concatenation = str1 + str2;
+console.log("Result-1b", concatenation);
+// Exercise-1c
+var myFirstStr = "I am";
+var mySecondStr = " a Pianist.";
+var strLength = myFirstStr.length + mySecondStr.length;
+console.log("Result-1c", strLength);
