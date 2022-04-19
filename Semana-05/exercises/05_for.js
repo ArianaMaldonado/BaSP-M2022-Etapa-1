0e6d5f9acec553898b971ca80f5159891dcceccb
Ariana Maldonado <arii.maldonado97@gmail.com>
2022-04-19 14:07:54 -0300

    BaSP-2022: functions solution

--- Semana-05/exercises/05_for.js
+++ Semana-05/exercises/05_for.js
@@ -2,13 +2,13 @@
 mostrar una alerta utilizando cada una de las palabras. */
 let instruments = ["guitar", "piano", "flute", "violin", "drums"];
 for (var i = 0; i < instruments.length; i++) {
-    //alert(instruments[i]);
+    alert(instruments[i]);
 }
 
 /* Exercise-5b: Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada
 palabra modificada. */
 for (var i = 0; i < instruments.length; i++) {
-    //alert(instruments[i].substring(0, 1).toUpperCase() + instruments[i].substring(1).toLowerCase());
+    alert(instruments[i].substring(0, 1).toUpperCase() + instruments[i].substring(1).toLowerCase());
 }
 
 /* Exercise-5c: Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo
@@ -17,7 +17,7 @@ cadena completa. */
 var sentence = "";
 for (var i = 0; i < instruments.length; i++) {
     sentence = sentence + instruments[i];
-    //alert(sentence);
+    alert(sentence);
 }
 
 /* Exercise-5d: Crear una array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de la
